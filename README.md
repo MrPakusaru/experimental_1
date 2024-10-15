4baaecf2ee057b9d4c3b6e0cabe290d2cbd9d1c0
MrPakusaru <108364849+mrpakusaru@users.noreply.github.com>
2024-10-15 20:35:16 +0300

    Маленькая обнова

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# experimental_1
+# Проект "experimental_1"
+## Напоминалка:
+Перед разработкой этого проекта на другом устройстве сделать эти шаги:
++ Создать файл .env
++ В файле env определить значения для:
+  * `ENV_TZ=`
+  * `ENV_MYSQL_ROOT_PASSWORD=`
+  * `ENV_MYSQL_DATABASE=`
+  * `ENV_MYSQL_USER=`
+  * `ENV_MYSQL_PASSWORD=`
--- client/code/js/contacts/f.js
+++ client/code/js/contacts/f.js
@@ -6,26 +6,31 @@ $(function() {
         year: $('input[name="year"]')
     };
     DateInputs.all.on('input',function () {
-        console.log([DateInputs.day.val(),DateInputs.month.val(),DateInputs.year.val()]);
         if(DateInputs.day.val()!=='' && DateInputs.month.val()!=='none') {
-
-
-
-            let correctDate = validateDate(DateInputs.day.val(), DateInputs.month.val(), DateInputs.year.val());
-
-            console.log('correct? - ' + correctDate)
-
-        } else { console.log('not full');}
-
-
+            showInvalidDate(validateDate(DateInputs.day.val(), DateInputs.month.val(), DateInputs.year.val()));
+        } else { showInvalidDate(undefined); }
     });
 });
 
+function showInvalidDate (e) {
+    switch (e) {
+        case true:
+            console.log('correct');
+            break;
+        case false:
+            console.log('no correct');
+            break;
+        case undefined:
+            console.log('not full');
+            break;
+    }
+}
 function validateDate(day, month, year) {
-    // Создаем объект Date с переданными значениями
-    const date = new Date(year, month, day);
+    // Создаем объект Date с переданными значениями. Если year пусто - то проверяем дату високосного года
+    const date = year!=='' ? new Date(year, month, day) : new Date(2000, month, day);
     // Проверяем, соответствует ли созданная дата введённым значениям
     return (date.getDate() === Number(day)) && (date.getMonth() === Number(month));
 }
 //TODO: при нажатии на сохранить если день или месяц не заполнены, должно высвечиваться предупреждение
 //TODO: также ошибка, если некорректная дата
+
