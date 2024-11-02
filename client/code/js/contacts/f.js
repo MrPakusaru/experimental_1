6607278d93cc5fb698d321524af1f83f800bbb98
MrPakusaru <pakusaru180@gmail.com>
2024-11-03 01:47:00 +0300

    TEMPORARY CODE для синхронизации кода между устройствами

--- client/code/js/contacts/f.js
+++ client/code/js/contacts/f.js
@@ -1,33 +1,31 @@
 $(function() {
-    let jQs = {
+    let DateInputs = {
+        all: $('input[name="day"], select[name="month"], input[name="year"]'),
         day: $('input[name="day"]'),
         month: $('select[name="month"]'),
-        year: $('input[name="year"]'),
-        all:$('input[name="day"], select[name="month"], input[name="year"]')
+        year: $('input[name="year"]')
     };
+    DateInputs.all.on('input',function () {
+        console.log([DateInputs.day.val(),DateInputs.month.val(),DateInputs.year.val()]);
+        if(DateInputs.day.val()!=='' && DateInputs.month.val()!=='none') {
 
-    if (jQs.month.val()==='none') console.log('jQs.month.val()');
 
-    jQs.all.on('input',function (e) {
 
-        console.log(jQs.month.val());
-        if (jQs.month.val()==='none') console.log('jQs.month.val()');
+            let correctDate = validateDate(DateInputs.day.val(), DateInputs.month.val(), DateInputs.year.val());
 
-        if(jQs.year.val()!=='' && jQs.month.val()!=='' && jQs.day.val()!=='') {
-            console.log([jQs.year.val(),jQs.month.val(),jQs.day.val()]);
-            console.log(
-                new Date(
-                    jQs.year.val().toString() + ' '
-                    + jQs.month.val() + ' '
-                    + jQs.day.val().toString()
-                )
-            );
+            console.log('correct? - ' + correctDate)
 
-        }
+        } else { console.log('not full');}
+
+
     });
 });
 
-
-//TODO сделать валидацию даты
-
-
+function validateDate(day, month, year) {
+    // Создаем объект Date с переданными значениями
+    const date = new Date(year, month, day);
+    // Проверяем, соответствует ли созданная дата введённым значениям
+    return (date.getDate() === Number(day)) && (date.getMonth() === Number(month));
+}
+//TODO: при нажатии на сохранить если день или месяц не заполнены, должно высвечиваться предупреждение
+//TODO: также ошибка, если некорректная дата
