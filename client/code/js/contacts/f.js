ea942743ce9dc4bd7caa5a1ea5d474eafd00e0c3
MrPakusaru <pakusaru180@gmail.com>
2025-02-05 02:25:16 +0300

    TEMPORARY CODE UPDATE Дописана логика подсветки полей ввода даты. Теперь поля реагируют на неправильный вид введённой даты

--- client/code/js/contacts/f.js
+++ client/code/js/contacts/f.js
@@ -1,36 +1,73 @@
 $(function() {
-    let DateInputs = {
-        all: $('input[name="day"], select[name="month"], input[name="year"]'),
+    let inputs = {
         day: $('input[name="day"]'),
         month: $('select[name="month"]'),
-        year: $('input[name="year"]')
+        year: $('input[name="year"]'),
+        all: $('input[name="day"], select[name="month"], input[name="year"]')
     };
-    DateInputs.all.on('input',function () {
-        if(DateInputs.day.val()!=='' && DateInputs.month.val()!=='none') {
-            showInvalidDate(validateDate(DateInputs.day.val(), DateInputs.month.val(), DateInputs.year.val()));
-        } else { showInvalidDate(undefined); }
+    inputs.all.on('change', () => checkFieldsOfDate(inputs));
+    $('div.window.contacts_table div.window_bar button').on('click', function () {
+        //TODO
     });
 });
+//TODO: добавить текст с подсказкой как у гугла.
+//TODO: В случае похожего окна добавить способ различать окна с похожими полями, чтобы не светились все вместе
+/**
+ * Меняет класс у полей day, month и year, тем самым включая и выключая их подсветку
+ * @param isValid
+ * @param elements
+ */
+const changeValidity = (isValid, elements) => {
+    elements.toggleClass('valid_input', isValid).toggleClass('invalid_input', !isValid);
+};
 
-function showInvalidDate (e) {
-    switch (e) {
-        case true:
-            console.log('correct');
-            break;
-        case false:
-            console.log('no correct');
-            break;
-        case undefined:
-            console.log('not full');
-            break;
-    }
+/**
+ * Подсвечивает поля day, month и year в зависимости от корректности введённой в них даты
+ * @param {any} fields
+ */
+function checkFieldsOfDate(fields) {
+    //Получает из полей классом day, month и year свойство value
+    const [day, month, year] = fields.all.toArray().map(el => el.value);
+    let actionMatrix = [
+        // Если все поля пустые, то подсветки нет
+        {
+            condition: () => year === '' && day === '' && month === 'none',
+            action: () => changeValidity(true, fields.all.parent())
+        },
+        // Если year пустой и есть ошибки в day или month, то в полях day и month подсветка
+        {
+            condition: () => year === '' && (day === '' || month === 'none'),
+            action: () => {
+                changeValidity(false, fields.all.not('[name="year"]').parent());
+                changeValidity(true, fields.year.parent());
+            }
+        },
+        // Если year, day и month заполнены корректно, то нет подсветки
+        {
+            condition: () => validateDate(day, month, year),
+            action: () => changeValidity(true, fields.all.parent())
+        },
+        // Все остальные случаи (если year, day и month заполнены корректно) - то все поля подсвечены
+        {
+            condition: () => true,
+            action: () => changeValidity(false, fields.all.parent())
+        }
+    ];
+    // Поиск и выполнение первого подходящего условия
+    actionMatrix.find(state => state.condition()).action();
 }
+
+
+/**
+ * Проверяет значение входящей даты, возвращает true, если полученная дата существует. Иначе возвращает false.
+ * @param day День
+ * @param month Месяц
+ * @param year Год
+ * @returns {boolean} Результат проверки
+ */
 function validateDate(day, month, year) {
-    // Создаем объект Date с переданными значениями. Если year пусто - то проверяем дату високосного года
+    // Создаёт объект Date с переданными значениями. Если year пусто - то проверяет дату високосного года
     const date = year!=='' ? new Date(year, month, day) : new Date(2000, month, day);
-    // Проверяем, соответствует ли созданная дата введённым значениям
+    // Проверяет, соответствует ли созданная дата введённым значениям
     return (date.getDate() === Number(day)) && (date.getMonth() === Number(month));
 }
-//TODO: при нажатии на сохранить если день или месяц не заполнены, должно высвечиваться предупреждение
-//TODO: также ошибка, если некорректная дата
-
